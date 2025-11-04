4a8ef5e030d1ec8be247f9a4ff64f19f55d3a269
1COWOO <kowoomccmd@gmail.com>
2025-11-04 12:49:55 +0000

    test

---
+++ src/client/java/com/github/kowoo/CopperAppleModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.kowoo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopperAppleModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
